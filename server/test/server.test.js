9830cfadbab46f07baab9386387505afb6d78916
durgaletchumy <durgaletchumy93@hotmail.com>
2018-05-09 22:25:54 +0800

    Add test for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -8,8 +8,16 @@ const {Todo} = require("./../models/todo");
 //This is to be run before it goes through the describe, which means it will
 //remove all the data in table before proceed to the next test.
 
+const todos = [{
+    text: 'First test todo'
+},{
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 //test script
@@ -29,7 +37,7 @@ describe("POST /todos", (req, res) => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done()
@@ -50,7 +58,7 @@ describe("POST /todos", (req, res) => {
     			}
 
     			Todo.find().then((todos) => {
-    				expect(todos.length).toBe(0);
+    				expect(todos.length).toBe(2);
     				done()
     			}).catch((e) => {
     				done(e)
@@ -58,3 +66,15 @@ describe("POST /todos", (req, res) => {
     		});
     });
 });
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    })
+})
